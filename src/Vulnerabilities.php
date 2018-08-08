91efcc4172d555c2f813e7857a3ab0f4cbceb1fe
Karim Mohamed <komicho1996@gmail.com>
2018-08-08 18:01:20 +0200

    test vulnerabilities to database

--- src/Vulnerabilities.php
+++ src/Vulnerabilities.php
@@ -4,5 +4,19 @@ namespace Shieldfy;
 
 class Vulnerabilities
 {
-    //
+    public function init($app_key, $app_secret)
+    {
+        $shieldfy = \Shieldfy\Guard::init([
+            'app_key'       => $app_key,
+            'app_secret'    => $app_secret
+        ]);
+    
+        $pdo = new PDO('sqlite:' . __DIR__ . '/mydatabase.db');
+    
+        $db = $shieldfy->attachPDO($pdo);
+    
+        $results = $db->query("SELECT joke FROM `users` where username = '". $_GET['username'] ."'");
+        $results = $results->fetchColumn();
+        echo json_encode($results);
+    }
 }
